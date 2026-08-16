5ad34195306645dd3debe66cc8a10ce934d2dcf5
Jason Jun <uxd.jun@gmail.com>
2026-08-16 13:51:21 +1000

    Restore DialKit slider dragging

--- Tools/ComponentEditor/src/main.tsx
+++ Tools/ComponentEditor/src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import { ComponentEditor } from "./ComponentEditor";
-import { installSliderPointerGuard } from "./slider-pointer-guard";
 import "./styles.css";
 
 const rootElement = document.getElementById("root");
@@ -8,5 +7,4 @@ if (rootElement === null) {
   throw new Error("Component Editor root element is missing.");
 }
 
-installSliderPointerGuard(document);
 createRoot(rootElement).render(<ComponentEditor />);
